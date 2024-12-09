5827ce92d1575dcff5633aabc40def975b9662fc
Clarissa Gunawan <gunawan.clarissa@outlook.com>
2024-12-09 10:22:35 -0800

    make delimiter and its size a const

--- protocol.py
+++ protocol.py
@@ -4,14 +4,16 @@ from dataclasses import dataclass
 class SimpleString():
     data: str
 
+DELIMITER = b"\r\n"
+DELIMITER_SIZE = 2
 
 def parse_frame(buffer):
-    end = buffer.find(b"\r\n")
+    end = buffer.find(DELIMITER)
     if end == -1:
         return None, 0
 
     match chr(buffer[0]):
         case "+":
-            return SimpleString(buffer[1: end].decode('ascii')), 5
+            return SimpleString(buffer[1: end].decode('ascii')), end + DELIMITER_SIZE
     
     return None, 0
