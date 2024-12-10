113aac17301cd8d59249ef19b8561f6d181ec0b9
Clarissa Gunawan <gunawan.clarissa@outlook.com>
2024-12-10 10:05:18 -0800

    create parse command to combine ping and echo command

--- pyredis/commands/commands.py
+++ pyredis/commands/commands.py
@@ -1,14 +1,29 @@
+from pyredis.protocol import parse_frame, SimpleString, Array
 
 
+def parse_command(buffer):
+    value, _ = parse_frame(buffer)
+    print(value)
 
-def ping_command(buffer):
-    if buffer:
-        return buffer
+    match value.data[0]:
+        case SimpleString("PING"):
+            return ping_command(value)
+        case SimpleString("ECHO"):
+            return echo_command(value)
 
-    return b"PONG"
+    return None, 0
+        
 
-def echo_command(buffer):
-    if buffer:
-        return buffer
+def ping_command(input):
+    if len(input.data) > 1:
+        input.data.pop(0)
+        return input.serialize()
+
+    return Array([SimpleString("PONG")]).serialize()
+
+def echo_command(input):
+    if len(input.data) > 1:
+        input.data.pop(0)
+        return input.serialize()
     
-    return b""
+    return Array([]).serialize()
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -6,27 +6,20 @@ REDIS Commands
 
 
 import pytest
-from pyredis.commands import ping_command, echo_command
+from pyredis.commands import parse_command
 
 
 @pytest.mark.parametrize(
         "buffer, expected",
         [
-            (b"", b"PONG"),
-            (b"Hello World", b"Hello World"),
+            # Ping Command
+            (b"*1\r\n+PING\r\n", b"*1\r\n+PONG\r\n"),
+            (b"*2\r\n+PING\r\n$11\r\nHello World\r\n", b"*1\r\n$11\r\nHello World\r\n"),
+            # Echo Command
+            (b"*1\r\n+ECHO\r\n", b"*0\r\n"),
+            (b"*2\r\n+ECHO\r\n$11\r\nHello World\r\n", b"*1\r\n$11\r\nHello World\r\n"),
         ]
 )
-def test_ping_command(buffer, expected):
-    got = ping_command(buffer)
-    assert got == expected
-
-
-@pytest.mark.parametrize(
-        "buffer, expected",
-        [
-            (b"Hello World", b"Hello World"),
-        ]
-)
-def test_echo_command(buffer, expected):
-    got = echo_command(buffer)
+def test_parse_command(buffer, expected):
+    got = parse_command(buffer)
     assert got == expected
