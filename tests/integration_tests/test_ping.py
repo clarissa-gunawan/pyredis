4f84f91826ee735bb5af66d438a3aa84540042a1
Clarissa Gunawan <clarissa.gunawan@sanctuary.ai>
2024-12-11 11:22:23 -0800

    add async and threaded server fixtures

--- tests/integration_tests/test_ping.py
+++ tests/integration_tests/test_ping.py
@@ -2,13 +2,13 @@
 import subprocess
 
 
-def test_ping(server):
+def test_ping(async_server, threaded_server):
     res = subprocess.run(["redis-cli", "-p", "6380", "PING"], stdout=subprocess.PIPE)
     assert res.returncode == 0
     assert res.stdout.decode("utf-8").strip() == "PONG"
 
 
-def test_ping_with_param(server):
+def test_ping_with_param(async_server, threaded_server):
     res = subprocess.run(["redis-cli", "-p", "6380", "PING", "HELLO"], stdout=subprocess.PIPE)
     assert res.returncode == 0
     assert res.stdout.decode("utf-8").strip() == "HELLO"
