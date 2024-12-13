cdce4aedf8c4a1f36dd2b57c959f8e464c0669ac
Clarissa Gunawan <gunawan.clarissa@outlook.com>
2024-12-12 21:06:21 -0800

    add lpush, rpush and lrange integration tests

---
+++ tests/integration_tests/test_xpush.py
@@ -0,0 +1,25 @@
+import subprocess
+
+
+def test_lpush(async_server):
+    res = subprocess.run(["redis-cli", "-p", "6380", "LPUSH", "message_lpush", "first_msg", "second_msg"], stdout=subprocess.PIPE)
+    assert res.returncode == 0
+    assert res.stdout.decode("utf-8").strip() == "2"
+
+
+def test_lpush_missing_argument(async_server):
+    res = subprocess.run(["redis-cli", "-p", "6380", "LPUSH"], stdout=subprocess.PIPE)
+    assert res.returncode == 0
+    assert res.stdout.decode("utf-8").strip() == "ERR wrong number of arguments for 'lpush' command"
+
+
+def test_rpush(async_server):
+    res = subprocess.run(["redis-cli", "-p", "6380", "RPUSH", "message_rpush", "first_msg"], stdout=subprocess.PIPE)
+    assert res.returncode == 0
+    assert res.stdout.decode("utf-8").strip() == "1"
+
+
+def test_rpush_missing_argument(async_server):
+    res = subprocess.run(["redis-cli", "-p", "6380", "RPUSH"], stdout=subprocess.PIPE)
+    assert res.returncode == 0
+    assert res.stdout.decode("utf-8").strip() == "ERR wrong number of arguments for 'rpush' command"
