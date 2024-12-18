1f49aeb678fef0c8236f8b8284e63c6ddc8f1b93
Clarissa Gunawan <gunawan.clarissa@outlook.com>
2024-12-18 10:54:20 -0800

    cleanup conftest

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -1,16 +1,6 @@
 import time
-
-from pyredis.datastore import QueueDataStore, Data
-
 import pytest
-
 import subprocess
-
-
-def setup_datastore():
-    ds = QueueDataStore()
-    ds.set("test_key", Data(value="test_value"))
-    return ds
 
 
 @pytest.fixture(scope="session")
